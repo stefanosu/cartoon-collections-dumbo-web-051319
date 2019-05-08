8d1bee515a1c74ee101550b2239a6a2988eabcdf
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-08 22:38:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,10 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find do |cheese|
+  if cheese_types.find do |cheese|
+    return true
+  else 
+    return nil 
+    end
+  end 
 end
