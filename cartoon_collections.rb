db9bcd081ff9ae08106902d77b5e502640dcc080
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-08 22:19:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+   arr.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer)
+    planteer.map do |planteers| 
+      puts planteer << "!" 
+  end
 end
 
 def long_planeteer_calls# code an argument here
